c34877e92c0359a315c5ce0086a17a878e23257b
Alexandre Philibert <alexandre.philibert@ik.me>
2025-01-18 14:24:32 +0100

    [Dialog] Add svelte form submission

--- src/routes/dialogs/+page.svelte
+++ src/routes/dialogs/+page.svelte
@@ -1,10 +1,17 @@
 <script>
+	import { enhance } from '$app/forms';
 	import Dialog from './dialog.svelte';
 	import { superForm } from 'sveltekit-superforms';
 
 	let { data } = $props();
 
-	const { form, errors, delayed, reset, enhance } = superForm(data.form, {
+	const {
+		form,
+		errors,
+		delayed,
+		reset,
+		enhance: sfEnhance
+	} = superForm(data.form, {
 		delayMs: 500
 	});
 </script>
@@ -59,14 +66,62 @@
 	<Dialog title="Form modal" onclose={reset}>
 		{#snippet trigger({ dialog })}
 			<button onclick={() => dialog.showModal()} class="rounded-md bg-neutral-300 p-1"
-				>form modal</button
+				>svelte form modal</button
 			>
 		{/snippet}
 		{#snippet children({ dialog })}
 			<form
 				class="flex flex-col"
 				method="POST"
-				use:enhance={{
+				use:enhance={() => {
+					alert('here');
+
+					return async ({ result }) => {
+						if (result.type === 'success') {
+							dialog.close();
+						}
+					};
+				}}
+			>
+				<p>This is the content of the modal</p>
+				<!-- svelte-ignore a11y_autofocus -->
+				<!-- We shouldn't get an a11y error inside a dialog -->
+				<input
+					type="text"
+					placeholder="text"
+					id="text"
+					name="text"
+					class="rounded-md border p-1"
+					autofocus
+					bind:value={$form.text}
+				/>
+				{#if $errors.text}
+					<div class="text-red-600">{$errors.text.join(',')}</div>
+				{/if}
+				<div class="mt-4 flex justify-end gap-2">
+					<button class="bg-neutral-300 p-1" formmethod="dialog">Cancel</button>
+					<button class="bg-neutral-300 p-1">
+						{#if $delayed}
+							...
+						{/if}
+						<span>Send</span>
+					</button>
+				</div>
+			</form>
+		{/snippet}
+	</Dialog>
+
+	<Dialog title="Form modal" onclose={reset}>
+		{#snippet trigger({ dialog })}
+			<button onclick={() => dialog.showModal()} class="rounded-md bg-neutral-300 p-1"
+				>sveltekit-superforms form modal</button
+			>
+		{/snippet}
+		{#snippet children({ dialog })}
+			<form
+				class="flex flex-col"
+				method="POST"
+				use:sfEnhance={{
 					onUpdated: () => {
 						dialog.close();
 					}
